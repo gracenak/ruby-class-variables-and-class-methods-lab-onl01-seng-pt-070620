c5ff0a524f6125146c0aeb0ebbbd324f29beed27
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-24 00:05:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,8 @@ class Song
     
   def self.genre_count
     genre_count = {}
-    @@genres.each {|genre| genre_count[genre]+= 1}
+    @@genres.each do |genre| 
+      if 
     genre_count
   end
 
